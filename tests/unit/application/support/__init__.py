ba18c3b34de5b18fe5a5cada4a8c3a12d6462d80
hamresan <pourshahmir@gmail.com>
2026-09-24 19:58:27 +0400

    Stage 5: export retry test fakes

--- tests/unit/application/support/__init__.py
+++ tests/unit/application/support/__init__.py
@@ -1,10 +1,17 @@
 from tests.unit.application.support.event_builder import build_event
 from tests.unit.application.support.processing_command import FakeProcessingCommand
-from tests.unit.application.support.retry_fakes import FakeEventQuery, FakeRetryScheduler
+from tests.unit.application.support.retry_fakes import (
+    FakeDueRetryReader,
+    FakeEventQuery,
+    FakeProcessingRunner,
+    FakeRetryScheduler,
+)
 
 __all__ = [
+    "FakeDueRetryReader",
     "FakeEventQuery",
     "FakeProcessingCommand",
+    "FakeProcessingRunner",
     "FakeRetryScheduler",
     "build_event",
 ]
