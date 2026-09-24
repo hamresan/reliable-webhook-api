8491b2252276e52acdc70f8a46994762ab5e090d
hamresan <pourshahmir@gmail.com>
2026-09-24 21:10:01 +0400

    Stage 6: strengthen clean architecture boundaries

--- src/reliable_webhook_api/application/ports/__init__.py
+++ src/reliable_webhook_api/application/ports/__init__.py
@@ -6,6 +6,7 @@ from reliable_webhook_api.application.ports.event_processor import EventProcesso
 from reliable_webhook_api.application.ports.event_query import EventPage, EventQuery
 from reliable_webhook_api.application.ports.event_repository import EventRepository
 from reliable_webhook_api.application.ports.metrics import Metrics
+from reliable_webhook_api.application.ports.readiness_checker import ReadinessChecker
 from reliable_webhook_api.application.ports.retry_scheduler import RetryScheduler
 from reliable_webhook_api.application.ports.signature_verifier import SignatureVerifier
 from reliable_webhook_api.application.ports.unit_of_work import UnitOfWork
@@ -20,6 +21,7 @@ __all__ = [
     "EventQuery",
     "EventRepository",
     "Metrics",
+    "ReadinessChecker",
     "RetryScheduler",
     "SignatureVerifier",
     "UnitOfWork",
