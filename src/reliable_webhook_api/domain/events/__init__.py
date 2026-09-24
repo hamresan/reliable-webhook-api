7b22cc30cfcbac41178b0d22b6d27b192f56695f
hamresan <pourshahmir@gmail.com>
2026-09-24 16:20:16 +0400

    refactor(domain): organize domain by responsibility

--- src/reliable_webhook_api/domain/events/__init__.py
+++ src/reliable_webhook_api/domain/events/__init__.py
@@ -1,13 +1,5 @@
 from reliable_webhook_api.domain.events.event_id import EventId
-from reliable_webhook_api.domain.events.failure_reason import FailureReason
-from reliable_webhook_api.domain.events.processing_attempt import ProcessingAttempt
 from reliable_webhook_api.domain.events.status import EventStatus
 from reliable_webhook_api.domain.events.webhook_event import WebhookEvent
 
-__all__ = [
-    "EventId",
-    "EventStatus",
-    "FailureReason",
-    "ProcessingAttempt",
-    "WebhookEvent",
-]
+__all__ = ["EventId", "EventStatus", "WebhookEvent"]
