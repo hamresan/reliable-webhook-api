38d8046cc3118733ed3479c0037d6e70e2e37237
hamresan <pourshahmir@gmail.com>
2026-09-24 18:00:47 +0400

    Stage 5: export processing command fake

--- tests/unit/application/support/__init__.py
+++ tests/unit/application/support/__init__.py
@@ -1,4 +1,10 @@
 from tests.unit.application.support.event_builder import build_event
+from tests.unit.application.support.processing_command import FakeProcessingCommand
 from tests.unit.application.support.retry_fakes import FakeEventQuery, FakeRetryScheduler
 
-__all__ = ["FakeEventQuery", "FakeRetryScheduler", "build_event"]
+__all__ = [
+    "FakeEventQuery",
+    "FakeProcessingCommand",
+    "FakeRetryScheduler",
+    "build_event",
+]
