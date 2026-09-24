a08a4769039a456fb9e4da6893f2f199159a6f4f
hamresan <pourshahmir@gmail.com>
2026-09-24 16:22:17 +0400

    refactor(domain): remove mixed events package

--- src/reliable_webhook_api/domain/events/__init__.py
+++
@@ -1,5 +0,0 @@
-from reliable_webhook_api.domain.events.event_id import EventId
-from reliable_webhook_api.domain.events.status import EventStatus
-from reliable_webhook_api.domain.events.webhook_event import WebhookEvent
-
-__all__ = ["EventId", "EventStatus", "WebhookEvent"]
