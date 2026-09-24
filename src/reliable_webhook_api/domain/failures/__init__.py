de1887da02c142640875e5127793338fcfdf193b
hamresan <pourshahmir@gmail.com>
2026-09-24 16:26:36 +0400

    refactor(domain): isolate failure value invariants

--- src/reliable_webhook_api/domain/failures/__init__.py
+++ src/reliable_webhook_api/domain/failures/__init__.py
@@ -1,3 +1,5 @@
+from reliable_webhook_api.domain.failures.failure_code import FailureCode
+from reliable_webhook_api.domain.failures.failure_message import FailureMessage
 from reliable_webhook_api.domain.failures.failure_reason import FailureReason
 
-__all__ = ["FailureReason"]
+__all__ = ["FailureCode", "FailureMessage", "FailureReason"]
