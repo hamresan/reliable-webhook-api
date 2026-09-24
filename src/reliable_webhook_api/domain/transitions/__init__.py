b2bb2d9b7111ecc90873434567d9b17fa036ae67
hamresan <pourshahmir@gmail.com>
2026-09-24 16:09:40 +0400

    feat(domain): add webhook event model and transition policy

---
+++ src/reliable_webhook_api/domain/transitions/__init__.py
@@ -0,0 +1,4 @@
+from reliable_webhook_api.domain.transitions.errors import InvalidEventTransitionError
+from reliable_webhook_api.domain.transitions.policy import EventTransitionPolicy
+
+__all__ = ["EventTransitionPolicy", "InvalidEventTransitionError"]
