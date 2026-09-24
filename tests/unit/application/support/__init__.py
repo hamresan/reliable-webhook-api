cbca64b3b565c0ea68d40731e6bff0920ec688af
hamresan <pourshahmir@gmail.com>
2026-09-24 20:01:52 +0400

    Stage 5: clean test support exports

--- tests/unit/application/support/__init__.py
+++ tests/unit/application/support/__init__.py
@@ -1,5 +1,4 @@
 from tests.unit.application.support.event_builder import build_event
-from tests.unit.application.support.processing_command import FakeProcessingCommand
 from tests.unit.application.support.retry_fakes import (
     FakeDueRetryReader,
     FakeEventQuery,
@@ -10,7 +9,6 @@ from tests.unit.application.support.retry_fakes import (
 __all__ = [
     "FakeDueRetryReader",
     "FakeEventQuery",
-    "FakeProcessingCommand",
     "FakeProcessingRunner",
     "FakeRetryScheduler",
     "build_event",
