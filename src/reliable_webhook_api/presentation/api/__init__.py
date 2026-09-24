92794d0a33bee3afc7ca666e8f36da374d29a1e3
hamresan <pourshahmir@gmail.com>
2026-09-24 15:44:53 +0400

    fix: replace escaped newlines with real line breaks

--- src/reliable_webhook_api/presentation/api/__init__.py
+++ src/reliable_webhook_api/presentation/api/__init__.py
@@ -1,1 +1,3 @@
-from reliable_webhook_api.presentation.api.app import app, create_app\n\n__all__ = ["app", "create_app"]\n
+from reliable_webhook_api.presentation.api.app import app, create_app
+
+__all__ = ["app", "create_app"]
