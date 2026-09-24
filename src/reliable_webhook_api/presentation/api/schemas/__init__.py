d464e06042b97dde4fad8168ca59a1c1e4c8f79f
hamresan <pourshahmir@gmail.com>
2026-09-24 17:57:27 +0400

    Stage 5: clean schema exports

--- src/reliable_webhook_api/presentation/api/schemas/__init__.py
+++ src/reliable_webhook_api/presentation/api/schemas/__init__.py
@@ -1,15 +1,19 @@
-from reliable_webhook_api.presentation.api.schemas.webhook import (
-    WebhookEventRequest,
-    WebhookEventResponse,
-)
-
-__all__ = ["WebhookEventRequest", "WebhookEventResponse"]
-
 from reliable_webhook_api.presentation.api.schemas.operations import (
     AttemptResponse,
     EventListResponse,
     EventResponse,
     RetryResponse,
 )
+from reliable_webhook_api.presentation.api.schemas.webhook import (
+    WebhookEventRequest,
+    WebhookEventResponse,
+)
 
-__all__ = [*__all__, "AttemptResponse", "EventListResponse", "EventResponse", "RetryResponse"]
+__all__ = [
+    "AttemptResponse",
+    "EventListResponse",
+    "EventResponse",
+    "RetryResponse",
+    "WebhookEventRequest",
+    "WebhookEventResponse",
+]
